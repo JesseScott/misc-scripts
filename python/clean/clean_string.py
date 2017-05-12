c9fcb7753fd3f6445d171ab3e21374482b8a0775
Jesse Scott <jesse.scott@eventbase.com>
2017-05-12 08:41:17 -0700

    added clean_string

---
+++ python/clean/clean_string.py
@@ -0,0 +1,7 @@
+
+def clean_text(text_string, special_characters):
+    cleaned_string = text_string
+    for string in special_characters:
+        cleaned_string = cleaned_string.replace(string, "")
+    cleaned_string = cleaned_string.lower()
+    return(cleaned_string)
